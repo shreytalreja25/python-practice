9cd22e7464311bd514ad9d0678c68a6b76c0d860
Shrey Talreja <shreytalreja25@gmail.com>
2023-02-12 15:10:27 +0530

    Adding some files

--- datatypes.py
+++ datatypes.py
@@ -1,3 +1,13 @@
 a = 10
 
 print(type(a))
+
+b = 10.5
+print(type(b))
+
+c = 'Shrey Talreja'
+
+print(type(c))
+
+d = [1,2,3,4]
+print(type(d))
---
+++ strings.py
@@ -0,0 +1,22 @@
+# Strings are charecters enclosed inside single or double quotes.
+
+str = 'This is a string'
+
+new_str = " This is also a string"
+
+multi_line_str = "  This is a multiline string \
+containing words spread over \
+few different lines."
+
+print(str)
+
+print(new_str)
+
+print(multi_line_str)
+
+name = "Shrey Talreja"
+
+print(name[2])
+name_len = str(len(name))
+
+print("Length of the name is : " + name_len)
