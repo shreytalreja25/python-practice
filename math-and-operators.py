de73277c4d5eb5c11302f181f734bd8cc51196c8
Shrey Talreja <shreytalreja25@gmail.com>
2023-02-12 17:14:36 +0530

    Math operators

---
+++ math-and-operators.py
@@ -0,0 +1,18 @@
+# basic math ops
+# +/-/*//
+# and , or , not
+
+a = False
+b = False
+
+if a and b == True:
+    print("Both True")
+
+elif a or b == True:
+    print("Either one is True")
+
+elif a and b == False:
+    print("Both False")
+
+elif a or b == False:
+    print("Either is False")
