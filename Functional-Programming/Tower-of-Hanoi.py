e2404f9c1b0b088c83aabf810ca293ff9c70c69d
Shrey Talreja <shreytalreja25@gmail.com>
2023-02-21 09:57:58 +0530

    Adding alternate approach to Tower of Hanoi

--- Functional-Programming/Tower-of-Hanoi.py
+++ Functional-Programming/Tower-of-Hanoi.py
@@ -9,4 +9,18 @@ def tower_of_hanoi(n, source, destination, intermediate):
         print(f"Move disk {n} from {source} to {destination}")
         tower_of_hanoi(n-1,intermediate,destination,source)
 
-tower_of_hanoi(64,"A","B","C")
+tower_of_hanoi(2,"A","B","C")
+
+# def TowerOfHanoiALT(n, from_rod, to_rod, aux_rod):
+#     if n == 0:
+#         return
+#     TowerOfHanoiALT(n-1, from_rod, aux_rod, to_rod)
+#     print("Move disk", n, "from rod", from_rod, "to rod", to_rod)
+#     TowerOfHanoiALT(n-1, aux_rod, to_rod, from_rod)
+  
+  
+# # Driver code
+# N = 2
+  
+# # A, C, B are the name of rods
+# TowerOfHanoiALT(N, 'A', 'C', 'B')
